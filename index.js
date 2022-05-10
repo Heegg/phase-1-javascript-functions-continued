6f3446dc7a245a0d8fcd09c92da5e869f1d45e34
Heeji Hyun <wlgmlgus@gmail.com>
2022-05-10 14:36:31 -0400

    commit tree 276c9ad21822d530a26ec2d44113d7e230c550bc

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun (play = "roller-skate") {
+    return `This Saturday, I want to ${play}!`
+}
+saturdayFun()
+
+function mondayWork (work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+mondayWork()
+
+
+
+
+function wrapAdjective(wrap = "special") {
+    const part1 = "*a hard worker*!"
+    return function (){
+        const part2 = "||a dedicated programmer||!"
+        console.log(`You are ${wrap}`)
+    }
+}
+wrapAdjective()
